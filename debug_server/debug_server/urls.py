a759318180528b0794ad8988686331a00632f20d
umezaki <bungoume3@yahoo.co.jp>
2015-03-22 18:55:18 +0900

    remove django admin

--- debug_server/debug_server/urls.py
+++ debug_server/debug_server/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'', 'webfront.views.index')
 ]
