092375208eccb33497232aeddfc4b8242be9026e
umezaki <bungoume3@yahoo.co.jp>
2015-03-24 00:08:36 +0900

    update tests

--- debug_server/webfront/tests.py
+++ debug_server/webfront/tests.py
@@ -13,3 +13,17 @@ class TestIndex(TestCase):
         self.assertEqual(res.status_code, 200)
         res_data = json.loads(res.content.decode())
         self.assertEqual(res_data['get']['param1'], ['test_param'])
+
+    def test_non_unicode_body(self):
+        body = "foo".encode('utf-16')
+        res = self.client.post(self._getTargetURL(), body, content_type="text/plain")
+        self.assertEqual(res.status_code, 200)
+        res_data = json.loads(res.content.decode())
+        self.assertEqual(res_data['body'], None)
+
+    def test_json_body(self):
+        body = json.dumps({'data': 'sample'})
+        res = self.client.post(self._getTargetURL(), body, content_type="application/json")
+        self.assertEqual(res.status_code, 200)
+        res_data = json.loads(res.content.decode())
+        self.assertEqual(res_data['body_json']['data'], 'sample')
