52c6e117f21efb983889432bd1d1746dfbf529d6
umezaki <bungoume3@yahoo.co.jp>
2015-05-11 00:08:34 +0900

    Add json error handler page

--- debug_server/debug_server/urls.py
+++ debug_server/debug_server/urls.py
@@ -3,3 +3,8 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'', 'webfront.views.index', name='index')
 ]
+
+handler400 = 'webfront.views.handler400'
+handler403 = 'webfront.views.handler403'
+handler404 = 'webfront.views.handler404'
+handler500 = 'webfront.views.handler500'
