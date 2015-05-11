a108d004c68aa23cb5960b7ccea0b4b38fd91a8a
umezaki <bungoume3@yahoo.co.jp>
2015-05-12 00:09:01 +0900

    Add tests

--- debug_server/webfront/tests.py
+++ debug_server/webfront/tests.py
@@ -28,3 +28,55 @@ class TestIndex(TestCase):
         self.assertEqual(res.status_code, 200)
         res_data = json.loads(res.content.decode())
         self.assertEqual(res_data['body_json']['data'], 'sample')
+
+
+class TestHandler400(TestCase):
+    def _callFUT(self, request):
+        from webfront.views import handler400
+        return handler400(request)
+
+    def test__it(self):
+        import json
+        res = self._callFUT('dummy request')
+        body = json.loads(res.content.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(body['error']['code'], 'bad_request')
+
+
+class TestHandler403(TestCase):
+    def _callFUT(self, request):
+        from webfront.views import handler403
+        return handler403(request)
+
+    def test__it(self):
+        import json
+        res = self._callFUT('dummy request')
+        body = json.loads(res.content.decode())
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(body['error']['code'], 'permission_denied')
+
+
+class TestHandler404(TestCase):
+    def _callFUT(self, request):
+        from webfront.views import handler404
+        return handler404(request)
+
+    def test__it(self):
+        import json
+        res = self._callFUT('dummy request')
+        body = json.loads(res.content.decode())
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(body['error']['code'], 'not_found')
+
+
+class TestHandler500(TestCase):
+    def _callFUT(self, request):
+        from webfront.views import handler500
+        return handler500(request)
+
+    def test__it(self):
+        import json
+        res = self._callFUT('dummy request')
+        body = json.loads(res.content.decode())
+        self.assertEqual(res.status_code, 500)
+        self.assertEqual(body['error']['code'], 'server_error')
