d19511cc9acfe3b296f94e919cb02663d9fd32d5
umezaki <bungoume3@yahoo.co.jp>
2015-05-15 23:17:48 +0900

    Add secret_key settings

--- debug_server/debug_server/settings/development.py
+++ debug_server/debug_server/settings/development.py
@@ -1,1 +1,5 @@
 from debug_server.settings import *  # NOQA
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or SECRET_KEY
+TIME_ZONE = os.environ.get('TIME_ZONE') or TIME_ZONE
--- debug_server/debug_server/settings/production.py
+++ debug_server/debug_server/settings/production.py
@@ -4,3 +4,6 @@ from debug_server.settings import *  # NOQA
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or SECRET_KEY
+TIME_ZONE = os.environ.get('TIME_ZONE') or TIME_ZONE
